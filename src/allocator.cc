8f1b7658193a18eb9bcc8b00bcc0d7d90ff4da45
wgtdkp <wgtdkp@163.com>
2017-01-07 15:52:04 +0800

    finish allocator

--- src/allocator.cc
+++ src/allocator.cc
@@ -1,56 +1,121 @@
 #include "allocator.h"
 
+#include <cstring>
+
 namespace nvds {
 
 uintptr_t Allocator::Alloc(uint32_t size) {
   // The client side should refuse too big kv item.
-  assert(size <= kMaxItemSize);
+  auto blk_size = size + sizeof(uint32_t);
+  assert(blk_size <= kMaxBlockSize);
 
-  return 0;
+  auto blk = AllocBlock(blk_size);
+  return blk == 0 ? 0 : blk + sizeof(uint32_t) + base_;
 }
 
 void Allocator::Free(uintptr_t ptr) {
   // TODO(wgtdkp): Checking if ptr is actually in this Allocator zone.
+  assert(ptr >= base_ + sizeof(uint32_t) && ptr <= base_ + kSize);
+  auto blk = ptr - sizeof(uint32_t) - base_;
+  FreeBlock(blk);
+}
 
+void Allocator::Format() {
+  static_assert(sizeof(FreeListManager) == 512 * 1024,
+                "Wrong FreeListManager size");
+  memset(flm_, 0, sizeof(FreeListManager));
+  auto free_list = (kNumFreeList - 1) * sizeof(uint32_t);
+  auto blk = sizeof(FreeListManager);
+  // 'The block before first block' is not free
+  Write(blk, ~BlockHeader::kFreeMask & kMaxBlockSize);
+  for (; blk < kSize; blk += kMaxBlockSize) {
+    auto head = Read<uint32_t>(free_list);
+    if (head != 0) {
+      Write(head + offsetof(BlockHeader, prev), blk);
+    }
+    Write(blk + offsetof(BlockHeader, prev), static_cast<uint32_t>(0));    
+    Write(blk + offsetof(BlockHeader, next), head);
+    Write(blk + kMaxBlockSize - sizeof(uint32_t), kMaxBlockSize);
+    SetBlockFree(blk, kMaxBlockSize);
+    
+    Write(free_list, blk);
+  }
 }
 
 uint32_t Allocator::AllocBlock(uint32_t blk_size) {
-  auto& free_lists = flm_->free_lists;
   uint32_t idx = blk_size / 16 - 1;
   for (uint32_t i = idx; i < kNumFreeList; ++i) {
-    if (free_lists[idx] != 0) {
-      if (i == idx) {
-        auto next_blk =
-            Read<uint32_t>(free_lists[idx] + offsetof(BlockHeader, next));
-        auto ret = free_lists[idx];
-        // FIXME(wgtdkp): assume little endian.
-        Write(free_lists[idx] + blk_size,
-              (~(static_cast<uint32_t>(1) << 31)) &
-                Read<uint32_t>(free_lists[idx] + blk_size));
-        // FIXEME(wgtdkp): totally wrong!!!
-        //Write(free_lists[idx], next_blk);
-        Write(next_blk + offsetof(BlockHeader, prev),
-              static_cast<uint32_t>(0));
-        return ret;
-      } else {
-        return SplitBlock(free_lists[idx], blk_size);
-      }
+    auto free_list = i * sizeof(uint32_t);
+    auto head = Read<uint32_t>(free_list);
+    auto head_size = (i + 1) * 16;
+    if (head == 0)
+      continue;
+    if (i == idx) {
+      auto next_blk = Read<uint32_t>(head + offsetof(BlockHeader, next));
+      // FIXME(wgtdkp): assume little endian.
+      ResetBlockFree(head, head_size);
+      Write(free_list, next_blk);
+      Write(next_blk + offsetof(BlockHeader, prev),
+            static_cast<uint32_t>(0));
+      return head;
+    } else {
+      return SplitBlock(head, head_size, blk_size);
     }
   }
   return 0;
 }
 
-void Allocator::FreeBlock(uint32_t blk_ptr) {
+void Allocator::FreeBlock(uint32_t blk) {
+  auto blk_size = Read<uint32_t>(blk) & ~BlockHeader::kFreeMask;
+  auto free_list = GetFreeListByBlockSize(blk_size);
+  auto head = Read<uint32_t>(free_list);
+  
+  auto prev_blk_size = Read<uint32_t>(blk - sizeof(uint32_t));
+  auto prev_blk = blk - prev_blk_size;
+  bool prev_blk_free = Read<uint32_t>(blk) & BlockHeader::kFreeMask;
+  auto next_blk = blk + blk_size;
+  auto next_blk_size = Read<uint32_t>(next_blk) & ~BlockHeader::kFreeMask;
+  bool next_blk_free = Read<uint32_t>(next_blk + next_blk_size) &
+                       BlockHeader::kFreeMask;
+  if (prev_blk_free) {
+    blk = prev_blk;
+    blk_size += prev_blk_size;
+  }
+  if (next_blk_free) {
+    blk_size += next_blk_size;
+  }
 
+  Write(free_list, blk);
+
+  SetBlockFree(blk, blk_size);
+  Write(blk + offsetof(BlockHeader, prev), static_cast<uint32_t>(0));
+  Write(blk + offsetof(BlockHeader, next), head);
+  if (head != 0) {
+    Write(head + offsetof(BlockHeader, prev), blk);
+  }
 }
 
-uint32_t Allocator::SplitBlock(uint32_t blk_offset, uint32_t needed_size) {
-  auto blk = OffsetToPtr<const BlockHeader>(blk_offset);
-  auto new_size = blk->size - needed_size;
-  Write(blk_offset, (static_cast<uint32_t>(blk->free) << 31) |
-                    (~(static_cast<uint32_t>(1) << 31) & new_size));
-  Write(blk_offset + new_size - sizeof(uint32_t), new_size);
-  Write(blk_offset + new_size, static_cast<uint32_t>(1) << 31 | needed_size);
+uint32_t Allocator::SplitBlock(uint32_t blk,
+                               uint32_t blk_size,
+                               uint32_t needed_size) {
+  auto next_blk = blk + blk_size;
+  auto free_list = GetFreeListByBlockSize(blk_size);
+  auto new_size = blk_size - needed_size;
+
+  Write(free_list, next_blk);
+  Write(next_blk + offsetof(BlockHeader, prev), static_cast<uint32_t>(0));
+
+  // Update size
+  Write(blk, (Read<uint32_t>(blk) & BlockHeader::kFreeMask) |
+             (~BlockHeader::kFreeMask & new_size));
+  Write(blk + new_size - sizeof(uint32_t), new_size);
+  // Free the block after updating size
+  FreeBlock(blk);
+
+  // The rest smaller block is still free  
+  Write(blk + new_size, BlockHeader::kFreeMask | needed_size);
+  ResetBlockFree(blk, blk_size);
+  return blk;
 }
 
 } // namespace nvds
--- src/allocator.h
+++ src/allocator.h
@@ -7,22 +7,24 @@ namespace nvds {
 
 class Allocator {
  public:
+  static const uint32_t kMaxBlockSize = 1024 * 1024;
   static const uint32_t kSize = 64 * 1024 * 1024;
+  Allocator(uintptr_t base, uint32_t size)
+    : base_(base), size_(size) {
+    flm_ = OffsetToPtr<FreeListManager>(0);
+  }
   ~Allocator() {}
   DISALLOW_COPY_AND_ASSIGN(Allocator);
 
   uintptr_t Alloc(uint32_t size);
   void Free(uintptr_t ptr);
-
+  void Format();
+  
   uintptr_t base() const { return base_; }
   uint32_t size() const { return size_; }
 
  private:
   static const uint32_t kNumFreeList = 2 * kMaxItemSize / 16;
-  Allocator(uintptr_t base, uint32_t size)
-    : base_(base), size_(size) {
-    flm_ = OffsetToPtr<FreeListManager>(0);
-  }
 
   template<typename T>
   T* OffsetToPtr(uint32_t offset) const {
@@ -37,9 +39,24 @@ class Allocator {
   }
 
   template<typename T>
+  void Write(T* ptr, T val) {
+    *ptr = val;
+    // TODO(wgtdkp): Collect
+  }
+
+  template<typename T>
   T Read(uint32_t offset) {
     return *OffsetToPtr<T>(offset);
   }
+
+  template<typename T>
+  class PMemObj {
+   public:
+    const PMemObj<T>& operator=(const T& other) {
+      static_cast<T>(*this) = other;
+      // TODO(wgtdkp): Collect
+    }
+  };
 
   PACKED(
   struct BlockHeader {
@@ -54,6 +71,7 @@ class Allocator {
     // Relative to base_.
     uint32_t next;
 
+    static const uint32_t kFreeMask = static_cast<uint32_t>(1) << 31;
     BlockHeader() {}
     ~BlockHeader() {}
   });
@@ -74,9 +92,28 @@ class Allocator {
     uint32_t free_lists[kNumFreeList];
   });
 
+  uint32_t GetFreeListByBlockOffset(uint32_t blk_offset) {
+    auto blk_size = OffsetToPtr<const BlockHeader>(blk_offset)->size;
+    return GetFreeListByBlockSize(blk_size);
+  }
+
+  uint32_t GetFreeListByBlockSize(uint32_t blk_size) {
+    return (blk_size / 16 - 1) * sizeof(uint32_t);
+  }
+
+  void SetBlockFree(uint32_t blk, uint32_t blk_size) {
+    auto next_blk = blk + blk_size;
+    Write(next_blk, (static_cast<uint32_t>(1) << 31) |
+                    Read<uint32_t>(next_blk));
+  }
+  void ResetBlockFree(uint32_t blk, uint32_t blk_size) {
+    auto next_blk = blk + blk_size;
+    Write(next_blk, ~(static_cast<uint32_t>(1) << 31) &
+                    Read<uint32_t>(next_blk));
+  }
   uint32_t AllocBlock(uint32_t size);
-  void FreeBlock(uint32_t ptr);
-  uint32_t SplitBlock(uint32_t blk_offset, uint32_t needed_size);
+  void FreeBlock(uint32_t blk);
+  uint32_t SplitBlock(uint32_t blk, uint32_t blk_size, uint32_t needed_size);
 
  private:
   uintptr_t base_;
