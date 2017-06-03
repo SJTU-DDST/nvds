333e825d0c0b25fd920dcc7d45deeba1282e4a7f
wgtdkp <wgtdkp@163.com>
2017-06-03 14:05:14 +0800

    fix bug: string replacement in startup script

--- script/startup.sh
+++ script/startup.sh
@@ -6,14 +6,15 @@
 sleep 0.1s
 
 first_line=$(head -n 1 coordinator.txt)
-coord_addr=($first_line)[-1]
+first_line_arr=($first_line)
+coord_addr=${first_line_arr[-1]}
 
 while read line; do
     server=($line)
-    dir='/home/wgtdkp/nvds/'
-    bin='./src/build/server'
-    out='./script/server_${server[1]}.txt'
-    err='./script/err_server_${server[1]}.txt'
-    in='/dev/null'
-    ssh root@${server[0]} 'cd ${dir}; nohup ${bin} ${server[1]} ${coord_addr} > ${out} 2> ${err} < ${in} &'
+    dir="/home/wgtdkp/nvds/"
+    bin="./src/build/server"
+    out="./script/server_${server[1]}.txt"
+    err="./script/err_server_${server[1]}.txt"
+    in="/dev/null"
+    ssh root@${server[0]} "cd ${dir}; nohup ${bin} ${server[1]} ${coord_addr} > ${out} 2> ${err} < ${in} &"
 done < servers.txt
